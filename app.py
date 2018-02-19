41e1f44a9cdec348df0f095136b929bfb7730d9a
Mark Jung <gujung2022@u.northwestern.edu>
2018-02-19 04:28:10 -0600

    changed app to application

--- app.py
+++ app.py
@@ -3,21 +3,21 @@ from flask_cors import CORS
 from flask_restful import Api
 from resources.quiz import Quiz
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+application = Flask(__name__)
+application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 #app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=2592000)
 #app.secret_key = ''
-api = Api(app)
-CORS(app)
+api = Api(application)
+#CORS(app)
 
 api.add_resource(Quiz, '/quiz')
 
 
 if __name__ == '__main__':
     from db import db
-    db.init_app(app)
-    @app.before_first_request
+    db.init_app(application)
+    @application.before_first_request
     def create_tables():
         db.create_all()
     app.run(port=5000, debug=True)
