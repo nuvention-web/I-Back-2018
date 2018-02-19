768d17a151fb7412cb78857856e9a6f13126d20f
Mark Jung <gujung2022@u.northwestern.edu>
2018-02-19 13:34:03 -0600

    sample

--- application.py
+++ application.py
@@ -10,6 +10,29 @@ application.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 #app.secret_key = ''
 api = Api(application)
 #CORS(app)
+# print a nice greeting.
+def say_hello(username = "World"):
+    return '<p>Hello %s!</p>\n' % username
+
+# some bits of text for the page.
+header_text = '''
+    <html>\n<head> <title>EB Flask Test</title> </head>\n<body>'''
+instructions = '''
+    <p><em>Hint</em>: This is a RESTful web service! Append a username
+    to the URL (for example: <code>/Thelonious</code>) to say hello to
+    someone specific.</p>\n'''
+home_link = '<p><a href="/">Back</a></p>\n'
+footer_text = '</body>\n</html>'
+
+# add a rule for the index page.
+application.add_url_rule('/', 'index', (lambda: header_text +
+    say_hello() + instructions + footer_text))
+
+# add a rule when the page is accessed with a name appended to the site
+# URL.
+application.add_url_rule('/<username>', 'hello', (lambda username:
+    header_text + say_hello(username) + home_link + footer_text))
+
 
 api.add_resource(Quiz, '/quiz')
 
@@ -20,5 +43,6 @@ if __name__ == '__main__':
     @application.before_first_request
     def create_tables():
         db.create_all()
-    application.run(port=5000, debug=True)
+    application.debug = True
+    application.run()
 
