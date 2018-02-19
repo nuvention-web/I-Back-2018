0cffcfba1e8346dac776bbd424c4b8cc257532c0
San Lee <sanlee2017@u.northwestern.edu>
2018-02-19 02:18:47 -0600

    change back to int

--- resources/quiz.py
+++ resources/quiz.py
@@ -3,25 +3,25 @@ from flask_restful import Resource, reqparse
 from controllers.quiz import QuizController
 
 class Quiz(Resource):
-    
+
     parser = reqparse.RequestParser()
     parser.add_argument('ans1',
-            type=str,
+            type=int,
             required=True,
             help="This field is required and cannot be left blank."
             )
     parser.add_argument('ans2',
-            type=str,
+            type=int,
             required=True,
             help="This field is required and cannot be left blank."
             )
     parser.add_argument('ans3',
-            type=str,
+            type=int,
             required=True,
             help="This field is required and cannot be left blank."
             )
     parser.add_argument('ans4',
-            type=str,
+            type=int,
             required=True,
             help="This field is required and cannot be left blank."
             )
@@ -29,10 +29,9 @@ class Quiz(Resource):
     def post(self):
         data = Quiz.parser.parse_args()
 
-        error_message, status, response = QuizController.respond_quiz(int(data['ans1']), int(data['ans2']), int(data['ans3']), int(data['ans4']))
+        error_message, status, response = QuizController.respond_quiz((data['ans1']), (data['ans2']), (data['ans3']), (data['ans4']))
 
         if error_message:
             return {"error_message": error_message}, status
 
         return {"response": response}, status
-
