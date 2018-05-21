a7e81808d6ea238d7d84d673aa33efc77103c972
Mark Jung <gujung2022@u.northwestern.edu>
2018-05-20 22:53:18 -0500

    local db setup for san is done

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from resources.notbought import NotBought
 from resources.bought import Bought
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///localdata.db')
 #app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 #app.config['JWT_EXPIRATION_DELTA'] = timedelta(seconds=2592000)
